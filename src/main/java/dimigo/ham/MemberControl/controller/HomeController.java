f39a8c8d6578a186ce5288433596f46c763716a9
ZZalbs <57441250+ZZalbs@users.noreply.github.com>
2020-12-19 22:03:26 +0900

    뷰 1차개편

--- src/main/java/dimigo/ham/MemberControl/controller/HomeController.java
+++ src/main/java/dimigo/ham/MemberControl/controller/HomeController.java
@@ -2,12 +2,18 @@ package dimigo.ham.MemberControl.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
     // 첫화면에서 선택 화면으로 가는 컨트롤러입니다.
     @GetMapping("/home")
     public String home()
+    {
+        return "home";
+    }
+    @PostMapping("/home")
+    public String home2()
     {
         return "home";
     }
